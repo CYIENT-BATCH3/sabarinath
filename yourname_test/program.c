0551417dc1b00a126b8ebd7427bbe57e5010f315
sabarinathKA <111974161+sabarinathKA@users.noreply.github.com>
2022-08-25 18:08:21 +0530

    Create program.c

---
+++ yourname_test/program.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(void)
+{
+  int num1,num2,addi,subs;
+  float div;
+  printf("enter 2 number");
+  scanf("%d%d",&numa,&num2);
+  addi = num1 + num2;
+  subs = num1 - num2;
+  div = (float)(num1/num2);
+  printf("sum = %d",addi);
+  printf("subs = %d",subs);
+  printf("div = %f",div);
+}
+  
