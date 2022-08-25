753376f9dab2808c11f057f06bd5d6c6eef77d1e
sabarinathKA <111974161+sabarinathKA@users.noreply.github.com>
2022-08-25 18:10:41 +0530

    Update program.c

--- yourname_test/program.c
+++ yourname_test/program.c
@@ -1,15 +1,17 @@
 #include<stdio.h>
 int main(void)
 {
-  int num1,num2,addi,subs;
+  int num1,num2,addi,subs,mul;
   float div;
   printf("enter 2 number");
   scanf("%d%d",&numa,&num2);
   addi = num1 + num2;
   subs = num1 - num2;
   div = (float)(num1/num2);
+  mul = num1 * num2;
   printf("sum = %d",addi);
   printf("subs = %d",subs);
   printf("div = %f",div);
+  printf("mul = %d",mul);
 }
   
