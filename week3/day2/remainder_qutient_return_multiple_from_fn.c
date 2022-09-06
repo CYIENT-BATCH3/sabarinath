c424a99f756d120f43a4a5ad4809809307cc12fc
sabarinathKA <111974161+sabarinathKA@users.noreply.github.com>
2022-09-06 21:26:52 +0530

    Update remainder_qutient_return_multiple_from_fn.c

--- week3/day2/remainder_qutient_return_multiple_from_fn.c
+++ week3/day2/remainder_qutient_return_multiple_from_fn.c
@@ -15,7 +15,7 @@ void main()
     printf("\nenetr the 2 number= ");
     scanf("%d%d",&num1,&num2);                          //taking user input from user
     divider(num1,num2,&ans,&remain);                                  //pass the reference of 2 variable to swap function
-     printf("\%d / %d  = %d  \n %d %% %d = %d",num1,num2,ans,num1,num2,remain);
+     printf("\n%d / %d  = %d  \n %d %% %d = %d",num1,num2,ans,num1,num2,remain);
 }
 
 void divider(int x,int y,int* i,int* j)                                //fn to find the remainder and answer the 2 values
